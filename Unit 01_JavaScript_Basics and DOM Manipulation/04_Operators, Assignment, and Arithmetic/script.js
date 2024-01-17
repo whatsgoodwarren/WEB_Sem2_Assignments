a3353fc0eb986d3fc4371ae6ae6f76d0dce89916
Caleb Nzao <nzaocal000@warrem.k12.in.us>
2024-01-17 08:46:50 -0500

    Assigment

---
+++ Unit 01_JavaScript_Basics and DOM Manipulation/04_Operators, Assignment, and Arithmetic/script.js
@@ -0,0 +1,20 @@
+let number1 = prompt("Enter a number: ");
+let number2 = prompt("Enter other number");
+
+number1 = +number1;
+number2 = +number2;
+
+console.log(typeof(number1));
+
+sum = number1 + number2;
+dif = number1 - number2;
+pro = number1 * number2;
+quo = number1 / number2;
+mod = number1 % number2;
+ 
+console.log(typeof(sum));
+console.log(typeof(dif));
+console.log(typeof(pro));
+console.log(typeof(quo));
+console.log(typeof(mod));
+
