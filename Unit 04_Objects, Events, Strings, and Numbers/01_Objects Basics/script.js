860298450a0359985952ef7144cd1b5a49caca02
Caleb Nzao <nzaocal00@warren.k12.in.us>
2024-02-15 10:39:34 -0500

    Assigment

---
+++ Unit 04_Objects, Events, Strings, and Numbers/01_Objects Basics/script.js
@@ -0,0 +1,19 @@
+const building = {
+    first:"Windows",
+    secound:"Doors",
+    third:"Walls",
+    id: 1234,
+    fullBuilding: function (){
+        return this.first + " " + this.secound + " " + this.third;
+    }
+    
+};
+
+console.log(person.first);
+console.log(person.secound);
+console.log(person.third);
+
+console.log(person.id);
+
+console.log(person.fullBuilding());
+
