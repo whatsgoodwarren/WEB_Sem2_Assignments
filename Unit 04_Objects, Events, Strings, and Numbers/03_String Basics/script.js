9b8c0c80e5e2f947cdd39411874765919ee56b81
Caleb Nzao <nzaocal000@warrem.k12.in.us>
2024-02-23 08:38:23 -0500

    Assigemn

--- Unit 04_Objects, Events, Strings, and Numbers/03_String Basics/script.js
+++ Unit 04_Objects, Events, Strings, and Numbers/03_String Basics/script.js
@@ -1,15 +1,10 @@
 let x = 5;
 
 let lyrics = `
-[MAJOR-GENERAL] 
-I am the very ${x}, model of a modern Major-General
-I've information vegetable, animal, and mineral
-I know the kings of England, and I quote the fights historical
-From Marathon to Waterloo, in order categorical
-I'm very well acquainted, too, with matters mathematical
-I understand equations, both the simple and quadratical
-About binomial theorem I am teeming with a lot o' news
-With many cheerful facts about the square of the hypotenuse
+Hey there it's nice to finally meet you
+And that your done hiding from under my bed
+I've been dancing in my bathroom
+Cause it's better than thinking of you instead
 `;
 
 document.getElementById("lyrics").innerHTML = lyrics;
