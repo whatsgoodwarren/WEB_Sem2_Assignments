48191bfe8870808c8c240e17b19fb5da5c6c2d4f
Caleb Nzao <nzaocal000@warrem.k12.in.us>
2024-03-18 08:41:03 -0400

    Assigment

---
+++ Unit 07_JavaScript Control Structures/02_For Loops/script.js
@@ -0,0 +1,12 @@
+const horses = ["Sally", "StanP", " Rebekah"];
+ 
+let text = " ";
+for(let x of horses){
+    text += x;
+    document.getElementById("demo").innerHTML = horses;      
+}
+
+const horse = {
+firstName: Sally
+
+};
